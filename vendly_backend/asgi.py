2a147bbee6cc46ce7947e3589c2be8d8f0aaced8
umakaran uma <umakaranuma1126@gmail.com>
2026-03-13 01:10:58 +0530

    Update environment configuration and load .env variables in Django settings. Refactor database settings to use environment variables for better security and flexibility.

--- vendly_backend/asgi.py
+++ vendly_backend/asgi.py
@@ -3,8 +3,13 @@ ASGI config for the vendly_backend project.
 """
 
 import os
+from pathlib import Path
 
+from dotenv import load_dotenv
 from django.core.asgi import get_asgi_application
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vendly_backend.settings.base")
 
